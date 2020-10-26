0167031bab36dfe33bf273cda683e66c230c3a59
math-rodr <59507657+math-rodr@users.noreply.github.com>
2020-10-25 22:59:23 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:notas_diarias/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
